f804c0e2ae57c6ec7aa8eaaea42cc45634187f96
MARCONDES J. Machado Dev <139270755+marcondesjm@users.noreply.github.com>
2024-07-02 13:04:16 -0300

    Add files via upload

--- src/pages/contactos/contactos.components/InfoContactos.jsx
+++ src/pages/contactos/contactos.components/InfoContactos.jsx
@@ -8,11 +8,13 @@ function InfoContactos() {
   return (
     <div className='infoscontactos'>
       <h2>Informações de Contato</h2>
-      <span><MdEmail className='ico-contactos'/>- marcondes.machado.ti@gmail.com</span>
-      <span className='tel-contacto'><MdPhoneIphone className='ico-contactos'/>- 48 99602 9392</span>
+      <span><MdEmail className='ico-contactos'/>- casadostoldosvendas10@hotmail.com</span>
+      <span className='tel-contacto'><MdPhoneIphone className='ico-contactos'/>- 48 9 9617-8175</span>
       <span><MdLocationOn className='ico-contactos'/>- São José
      - SC / Brasil</span>
+     
     </div>
+    
   )
 }
 
